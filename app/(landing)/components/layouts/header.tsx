3779f088b1811eb8b1f23a50623e2754688acc0d
fuazim <fauzimuh321@gmail.com>
2026-01-13 15:16:04 +0700

    add shopping cart

--- app/(landing)/components/layouts/header.tsx
+++ app/(landing)/components/layouts/header.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FiSearch, FiShoppingBag } from 'react-icons/fi'
+import CartPopup from '../ui/cartpopup'
+import { useState } from 'react'
+import Button from '../ui/button'
 
-export default function header() {
-  return (
-    <header className='absolute top-0 left-0 right-0 z-50'>
+const Header = () => {
+    const [isCartOpen, setIsCartOpen] = useState(false)
+    return (
+        <header className='absolute top-0 left-0 right-0 z-50'>
         <div className='flex justify-between gap-10 container mx-auto py-7'>
             <Image src="/logos/sporton-logo.svg" alt="Sporton Logo" width={127} height={30} />
             <nav className='flex gap-20 justify-center items-center font-medium'>
@@ -13,16 +17,20 @@ export default function header() {
                 <Link href="#">Category</Link>
                 <Link href="#">Explore Products</Link>
             </nav>
-            <div className='flex gap-10 items-center justify-center'>
+            <div className='relative flex gap-10 items-center justify-center'>
                 <FiSearch size={24}/>
-                <div className='relative'>
+                <Button variant="ghost" size="small" className='relative cursor-pointer p-0!' onClick={() => setIsCartOpen(!isCartOpen)}>
                     <FiShoppingBag size={24}/>
                     <div className='flex items-center justify-center absolute -top-1 -right-1 w-3.5 h-3.5 bg-primary rounded-full text-[10px] text-primary-light text-center'>
-                    3
+                        3
                     </div>
-                </div>
+                </Button>
+                <CartPopup open={isCartOpen} onClose={() => setIsCartOpen(false)} />
             </div>
         </div>
     </header>
   )
 }
+
+export default Header
+
